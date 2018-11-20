e7e3f22babbda9c8542d6f0349c7945b588ff1fe
Frank Denis <github@pureftpd.org>
2018-11-20 15:01:58 +0100

    Add some memory hardness

--- src/hashseq.c
+++ src/hashseq.c
@@ -12,6 +12,8 @@
 #include "common.h"
 #include "hashseq.h"
 
+#define HASHSEQ_LOCATIONS 8
+
 typedef struct Mask_ {
     uint32_t m0;
     uint32_t m1;
@@ -45,16 +47,41 @@ hash_init(uint32_t state[16], const uint32_t suffix[8], uint32_t level,
 }
 
 static int
-hash_try(uint32_t ostate[16], uint32_t istate[16],
+hash_try(HashSeqArena *arena, uint32_t ostate[16], uint32_t istate[16],
          const HashSeqSolution *proposal, const Mask *mask)
 {
-    uint32_t f0, f1;
-    int i;
+    uint32_t *locations[2 * HASHSEQ_LOCATIONS], *location1, *location2;
+    uint32_t  f0, f1;
+    uint32_t  pass, passes;
+    uint32_t  off;
+    size_t    i, j;
 
+    passes    = arena->size;
     istate[0] = IV[0] ^ proposal->s0;
     istate[1] = IV[1] ^ proposal->s1;
     memcpy(ostate, istate, 16 * sizeof ostate[0]);
-    for (i = 0; i < 6; i++) {
+    for (i = 0; i < 5; i++) {
+        permute(ostate);
+    }
+    for (pass = 0; pass < passes; pass++) {
+        for (i = 0; i < sizeof locations / sizeof locations[0]; i++) {
+            permute(ostate);
+            locations[i] = &arena->base[ostate[0] & arena->mask];
+        }
+        for (i = 0; i < sizeof locations / sizeof locations[0] / 2; i++) {
+            location1 = locations[i];
+            location2 =
+                locations[i + sizeof locations / sizeof locations[0] / 2];
+            permute(ostate);
+            for (j = 0; j < 16; j++) {
+                location1[i] ^= location2[i] + ostate[i];
+            }
+        }
+    }
+    for (off = 0; off < arena->size; off += 8 * sizeof ostate[0]) {
+        for (i = 0; i < 8; i++) {
+            ostate[i] ^= arena->base[off + i];
+        }
         permute(ostate);
     }
     f0 = ostate[0];
@@ -79,17 +106,17 @@ mask_from_level(Mask *mask, uint32_t level)
 }
 
 static int
-solve1(HashSeqSolution *solution, uint32_t suffix[8], uint32_t level,
-       uint32_t iteration)
+solve1(HashSeqArena *arena, HashSeqSolution *solution, uint32_t suffix[8],
+       uint32_t level, uint32_t iteration)
 {
-    uint32_t istate[16], ostate[16];
-    Mask mask;
+    uint32_t        istate[16], ostate[16];
+    Mask            mask;
     HashSeqSolution proposal;
 
     hash_init(istate, suffix, level, iteration);
     mask_from_level(&mask, level);
     proposal.s0 = proposal.s1 = 0U;
-    while (hash_try(ostate, istate, &proposal, &mask) == 0) {
+    while (hash_try(arena, ostate, istate, &proposal, &mask) == 0) {
         if (++proposal.s0 == 0U) {
             proposal.s1++;
         }
@@ -101,15 +128,15 @@ solve1(HashSeqSolution *solution, uint32_t suffix[8], uint32_t level,
 }
 
 static int
-verify1(const HashSeqSolution *proposal, uint32_t suffix[8], uint32_t level,
-        uint32_t iteration)
+verify1(HashSeqArena *arena, const HashSeqSolution *proposal,
+        uint32_t suffix[8], uint32_t level, uint32_t iteration)
 {
     uint32_t state[16];
-    Mask mask;
+    Mask     mask;
 
     hash_init(state, suffix, level, iteration);
     mask_from_level(&mask, level);
-    if (hash_try(state, state, proposal, &mask)) {
+    if (hash_try(arena, state, state, proposal, &mask)) {
         memcpy(suffix, &state[8], 8 * sizeof state[0]);
         return 1;
     }
@@ -117,17 +144,43 @@ verify1(const HashSeqSolution *proposal, uint32_t suffix[8], uint32_t level,
 }
 
 int
-hashseq_solve(HashSeqSolution *solutions, const uint32_t suffix[8],
-              uint32_t level_first, uint32_t level_last, uint32_t iterations)
+hashseq_arena_init(HashSeqArena *arena, uint32_t *base, size_t size)
+{
+    uint32_t mask;
+
+    if (size > (size_t)(uint32_t) -1) {
+        return -1;
+    }
+    memset(base, 0, size);
+    arena->base = base;
+    arena->size = (uint32_t) size;
+    mask        = 0U;
+    while (mask < (size >> 1)) {
+        mask = (mask << 1) | 1;
+    }
+    mask &= ~15U;
+    if (mask == 0U) {
+        return -1;
+    }
+    arena->mask = mask;
+
+    return 0;
+}
+
+int
+hashseq_solve(HashSeqArena *arena, HashSeqSolution *solutions,
+              const uint32_t suffix[8], uint32_t level_first,
+              uint32_t level_last, uint32_t iterations)
 {
     uint32_t suffix_[8];
     uint32_t level, iteration;
-    int i = 0;
+    int      i = 0;
 
     memcpy(suffix_, suffix, sizeof suffix_);
     for (level = level_first; level <= level_last; level++) {
         for (iteration = 0; iteration < iterations; iteration++) {
-            if (solve1(&solutions[i++], suffix_, level, iteration) != 0) {
+            if (solve1(arena, &solutions[i++], suffix_, level, iteration) !=
+                0) {
                 return -1;
             }
         }
@@ -136,17 +189,19 @@ hashseq_solve(HashSeqSolution *solutions, const uint32_t suffix[8],
 }
 
 int
-hashseq_verify(const HashSeqSolution *solutions, const uint32_t suffix[8],
-               uint32_t level_first, uint32_t level_last, uint32_t iterations)
+hashseq_verify(HashSeqArena *arena, const HashSeqSolution *solutions,
+               const uint32_t suffix[8], uint32_t level_first,
+               uint32_t level_last, uint32_t iterations)
 {
     uint32_t suffix_[8];
     uint32_t level, iteration;
-    int i = 0;
+    int      i = 0;
 
     memcpy(suffix_, suffix, sizeof suffix_);
     for (level = level_first; level <= level_last; level++) {
         for (iteration = 0; iteration < iterations; iteration++) {
-            if (verify1(&solutions[i++], suffix_, level, iteration) == 0) {
+            if (verify1(arena, &solutions[i++], suffix_, level, iteration) ==
+                0) {
                 return 0;
             }
         }
--- src/hashseq.h
+++ src/hashseq.h
@@ -4,10 +4,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct HashSeqArena_ {
+    uint32_t *base;
+    uint32_t  mask;
+    uint32_t  size;
+} HashSeqArena;
+
 typedef struct HashSeqSolution_ {
     uint32_t s0;
     uint32_t s1;
 } HashSeqSolution;
+
+/**
+ * Initialize the memory region used for computations.
+ */
+int hashseq_arena_init(HashSeqArena *arena, uint32_t *base, size_t size);
 
 /**
  * Solve the hash sequence for the random suffix `suffix`, from levels
@@ -17,9 +28,9 @@ typedef struct HashSeqSolution_ {
  * `solutions` will store (`level_last` - `level_first` + 1) * iterations
  * solutions.
  */
-int hashseq_solve(HashSeqSolution *solutions, const uint32_t suffix[8],
-                  uint32_t level_first, uint32_t level_last,
-                  uint32_t iterations);
+int hashseq_solve(HashSeqArena *arena, HashSeqSolution *solutions,
+                  const uint32_t suffix[8], uint32_t level_first,
+                  uint32_t level_last, uint32_t iterations);
 
 /**
  * Verify that the solutions `solutions` are valid for the suffix `suffix`,
@@ -28,8 +39,8 @@ int hashseq_solve(HashSeqSolution *solutions, const uint32_t suffix[8],
  *
  * Returns `1` on success, `0` if the solutions don't appear to be correct.
  */
-int hashseq_verify(const HashSeqSolution *solutions, const uint32_t suffix[8],
-                   uint32_t level_first, uint32_t level_last,
-                   uint32_t iterations);
+int hashseq_verify(HashSeqArena *arena, const HashSeqSolution *solutions,
+                   const uint32_t suffix[8], uint32_t level_first,
+                   uint32_t level_last, uint32_t iterations);
 
 #endif
